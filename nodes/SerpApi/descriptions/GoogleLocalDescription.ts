f2f1935427e7267cdd603f62f1b13d71bdd68b39
Alex Barron <alex@abarron.com>
2025-03-18 10:32:10 +0000

    Fix local location description

--- nodes/SerpApi/descriptions/GoogleLocalDescription.ts
+++ nodes/SerpApi/descriptions/GoogleLocalDescription.ts
@@ -28,7 +28,7 @@ export const googleLocalFields: INodeProperties[] = [
 		},
 	},
 	{
-		displayName: 'LOCAL`location` Location',
+		displayName: '`location` Location',
 		name: 'location',
 		description:
 			"Parameter defines from where you want the search to originate. If several locations match the location requested, we'll pick the most popular one. Head to the /locations.JSON API if you need more precise control. The location and uule parameters can't be used together. It is recommended to specify location at the city level in order to simulate a real user’s search. If location is omitted, the search may take on the location of the proxy.",
