118d457874ef190065d02136ca14cc4009eb6685
NateSkiles <nateskiles94@gmail.com>
2025-05-08 11:14:11 -0700

    fix linter errors

--- credentials/SerpApi.credentials.ts
+++ credentials/SerpApi.credentials.ts
@@ -1,17 +1,13 @@
-import {
-	IAuthenticateGeneric,
-	ICredentialType,
-	INodeProperties,
-	Icon
-} from 'n8n-workflow';
+import { IAuthenticateGeneric, ICredentialType, INodeProperties, Icon } from 'n8n-workflow';
 
 export class SerpApi implements ICredentialType {
-	name = 'serpapi';
+	name = 'serpApi';
 	displayName = 'SerpApi API';
 	icon: Icon = 'file:serpapi.svg';
 	// Uses the link to this tutorial as an example
 	// Replace with your own docs links when building your own nodes
-	documentationUrl = 'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
+	documentationUrl =
+		'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
 	properties: INodeProperties[] = [
 		{
 			displayName: 'API Key',
@@ -21,12 +17,12 @@ export class SerpApi implements ICredentialType {
 			default: '',
 		},
 	];
-	authenticate = {
+	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
 			qs: {
-				'api_key': '={{$credentials.apiKey}}'
-			}
+				api_key: '={{$credentials.apiKey}}',
+			},
 		},
-	} as IAuthenticateGeneric;
+	};
 }
