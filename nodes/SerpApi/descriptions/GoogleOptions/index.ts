e4b8be67b2913bc7093425cba6d94fa35e8352ff
NateSkiles <nateskiles94@gmail.com>
2025-05-07 18:29:38 -0700

    cleanup Google descriptions

---
+++ nodes/SerpApi/descriptions/GoogleOptions/index.ts
@@ -0,0 +1,5 @@
+import CountryOptions from './CountryOptions';
+import LanguageOptions from './LanguageOptions';
+import DomainOptions from './DomainOptions';
+
+export { CountryOptions, LanguageOptions, DomainOptions };
